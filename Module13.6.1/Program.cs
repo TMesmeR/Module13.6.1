b90377af48df9a42f0a73fc9ee2f85c57c6b0294
mesme <mesme@TMESMER>
2024-05-21 12:45:51 +0300

    Добавьте файлы проекта.

---
+++ Module13.6.1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Module13._6._1;
+
+string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Text1.txt");
+
+var tester = new ComparisonOnListAndLinkedList(filePath);
+tester.TestInsertion();
+
+
